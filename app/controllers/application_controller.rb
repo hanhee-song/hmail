ed5d26d9fbe185699fbcb01872c40237430df2f5
Hanhee Song <song.hanhee@gmail.com>
2017-12-16 14:38:05 -0500

    add user model and app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,38 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  helper_method :current_user, :logged_in?
+  
+  def current_user
+    if !@current_user
+      user = User.find_by(id: session[:auth_token])
+      if user && ActiveSupport::SecurityUtils.secure_compare(session[:auth_token], user.auth_token)
+        @current_user = user
+      end
+    end
+    @current_user
+  end
+  
+  def logged_in?
+    !!current_user
+  end
+  
+  def login!(user)
+    session[:auth_token] = user.reset_token!
+    session[:user_id] = user.id
+    @current_user = user
+  end
+  
+  def logout!
+    current_user.reset_token!
+    session[:auth_token] = nil
+    session[:user_id] = nil
+    @current_user = nil
+  end
+  
+  def ensure_login
+    if !current_user
+      render json: ["Please log in"], status: 403
+    end
+  end
 end
