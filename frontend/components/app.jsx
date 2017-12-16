656fd6ac1ef904b9167f0fbc4f834f5ed4b460be
Hanhee Song <song.hanhee@gmail.com>
2017-12-16 16:20:12 -0500

    add route utils

---
+++ frontend/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
+const App = () => {
+  return (
+    <div>hi</div>
+  );
+};
+
+export default App;
