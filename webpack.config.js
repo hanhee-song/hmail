ca02696e6a71fa5abb62808f8104458cb029a2b3
Hanhee Song <song.hanhee@gmail.com>
2017-12-16 15:08:49 -0500

    add package.json and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/hmail.jsx",
+  output: {
+    path: path.join(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  },
+    plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+  ]
+};
