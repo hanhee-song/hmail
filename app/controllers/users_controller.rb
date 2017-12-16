974d959d4f117c36d1586da9ce50976bbdc70a31
Hanhee Song <song.hanhee@gmail.com>
2017-12-16 15:28:54 -0500

    add session actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,17 +9,17 @@ class Api::UsersController < ApplicationController
     end
   end
 
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_param)
-      render "api/users/show"
-    else
-      render json: @user.errors.full_messages, status: 422
-    end
-  end
-
-  def show
-  end
+  # def update
+  #   @user = User.find(params[:id])
+  #   if @user.update(user_param)
+  #     render "api/users/show"
+  #   else
+  #     render json: @user.errors.full_messages, status: 422
+  #   end
+  # end
+  #
+  # def show
+  # end
   
   private
   
---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,55 @@
+import * as SessionApiUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+export const REMOVE_SESSION_ERRORS = "REMOVE_SESSION_ERRORS";
+
+export const receiveCurrentUser = (currentUser) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser,
+  };
+};
+
+export const receiveSessionErrors = (errors) => {
+  return {
+    type: RECEIVE_SESSION_ERRORS,
+    errors,
+  };
+};
+
+export const removeSessionErrors = () => {
+  return {
+    type: REMOVE_SESSION_ERRORS,
+  };
+};
+
+export const signup = (user) => {
+  return (dispatch) => {
+    return SessionApiUtil.signup(user)
+      .then(
+        user => dispatch(receiveCurrentUser(user)),
+        errors => dispatch(receiveSessionErrors(errors))
+      );
+  };
+};
+
+export const login = (user) => {
+  return (dispatch) => {
+    return SessionApiUtil.signup(user)
+      .then(
+        user => dispatch(receiveCurrentUser(user)),
+        errors => dispatch(receiveSessionErrors(errors))
+      );
+  };
+};
+
+export const logout = () => {
+  return (dispatch) => {
+    return SessionApiUtil.logout()
+      .then(
+        () => dispatch(receiveCurrentUser(null)),
+        errors => dispatch(receiveSessionErrors(errors))
+      );
+  };
+};
