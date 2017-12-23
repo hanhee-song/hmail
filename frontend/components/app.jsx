681f6bcdcc7e199e95273d155886292de331a24c
Hanhee Song <song.hanhee@gmail.com>
2017-12-23 12:03:42 -0500

    set up inbox navbar files

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,19 +5,21 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import Welcome from './welcome/welcome';
 import SignupContainer from './session/signup_container';
 import SigninContainer from './session/signin_container';
+import Inbox from './inbox/inbox';
 
 const App = () => {
   return (
     <section className="app">
       <Switch>
+        <ProtectedRoute
+          component={Inbox}
+          path="/inbox"/>
         <AuthRoute
           component={SignupContainer}
           path="/SignUp"/>
         <AuthRoute
           component={SigninContainer}
           path="/signin"/>
-        <ProtectedRoute
-          path="/inbox"/>
         <AuthRoute
           component={Welcome}
           path="/" />
@@ -27,3 +29,13 @@ const App = () => {
 };
 
 export default App;
+
+// inbox;
+//   navbar-top;
+//   navbar-bottom;
+//   sidebar;
+//     sidebar-index;
+//   message-index;
+//     message-index-header;
+//     message-index-items;
+//     message-index-footer;
