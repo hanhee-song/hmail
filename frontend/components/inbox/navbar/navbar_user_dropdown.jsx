9b076c40361b5de5aa126d5020cf91a0549cea11
Hanhee Song <song.hanhee@gmail.com>
2018-01-04 00:12:49 -0500

    style icon on user dropdown

--- frontend/components/inbox/navbar/navbar_user_dropdown.jsx
+++ frontend/components/inbox/navbar/navbar_user_dropdown.jsx
@@ -7,16 +7,20 @@ class NavbarUserDropdown extends React.Component {
   }
   
   render () {
+    const currentUser = this.props.currentUser;
     return (
       <div className="user-dropdown">
         <div className="user-dropdown-top">
-          <div className="user-dropdown-top-left"></div>
+          <div className="user-dropdown-top-left">
+            {currentUser.fname[0]}
+          </div>
+          
           <div className="user-dropdown-top-right">
             <div className="user-name">
-              Username
+              {currentUser.fname + " " + currentUser.lname}
             </div>
             <div className="user-email">
-              Email
+              {currentUser.email + "@hmail.com"}
             </div>
           </div>
         </div>
